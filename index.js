6659183d857debd224692ab35178baebb5a53da4
Jan Kaiponen <2504255+itsjan@users.noreply.github.com>
2024-10-10 09:31:46 +0300

    v 0.0.1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Hello World!</h1> <p>Versio 0.0.1')
 })
 
 const PORT = 8080
